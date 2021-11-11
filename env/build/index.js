6ba29c88c46c1b18e3d7d5dc3e3d365ee7d03aa4
henil0604 <henilmalaviya06@gmail.com>
2021-11-11 22:17:10 +0530

    build for env module

---
+++ env/build/index.js
@@ -0,0 +1,1 @@
+"use strict";require("dotenv").config();module.exports=function(key){var filter=arguments.length>1&&arguments[1]!==undefined?arguments[1]:true;var val=process.env[key];if(filter&&val=="true"){val=true}if(filter&&val=="false"){val=false}if(filter&&val=="null"){val=null}if(filter&&val=="undefined"){val=undefined}return val};
