c4372860d2c5bc10ec08ca73371c7be20fea3a47
henil0604 <henilmalaviya06@gmail.com>
2021-11-11 22:17:24 +0530

    added filter and without filter code

--- env/test/index.js
+++ env/test/index.js
@@ -1,4 +1,12 @@
-const env = require("../");
+const env = require("../build");
 
 // get variable from .env file
 console.log(env("name"));
+
+
+// with filter
+console.log(typeof env("logging"));
+
+
+// Without filter
+console.log(typeof env("logging", false))
