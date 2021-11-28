05165395634dd5f20c66e4604e8402f2bbd3844e
henil0604 <henilmalaviya06@gmail.com>
2021-11-28 18:53:48 +0530

    added "sleep" module

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let helperModules = {};
 
 helperModules.env = require("./env");
 helperModules.log = require("./log");
-
+helperModules.sleep = require("./sleep");
 
 
 module.exports = helperModules;
