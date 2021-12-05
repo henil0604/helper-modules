4386e30e29f837097b43640f58208c6fde0ea176
henil0604 <henilmalaviya06@gmail.com>
2021-12-05 23:11:29 +0530

    added "random" module

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let helperModules = {};
 helperModules.env = require("./env");
 helperModules.log = require("./log");
 helperModules.sleep = require("./sleep");
-
+helperModules.random = require("./random");
 
 module.exports = helperModules;
