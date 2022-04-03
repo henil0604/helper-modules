0f66413ea4e1f67743dab8c0391cbf92e47d44d3
henil0604 <henilmalaviya06@gmail.com>
2022-04-03 17:31:41 +0530

    added `throttle` module

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ helperModules.log = require("./log");
 helperModules.sleep = require("./sleep");
 helperModules.random = require("./random");
 helperModules.debounce = require("./debounce");
+helperModules.throttle = require("./throttle");
 
 module.exports = helperModules;
